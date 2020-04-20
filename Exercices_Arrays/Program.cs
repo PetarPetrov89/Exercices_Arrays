f9cbfc67da2b959f2ae9256c11d2517b55556a50
PetarPetrov89 <62606018+PetarPetrov89@users.noreply.github.com>
2020-04-20 16:22:04 +0300

    Exercices_Arrays

--- Exercices_Arrays/Program.cs
+++ Exercices_Arrays/Program.cs
@@ -6,7 +6,32 @@ namespace Exercices_Arrays
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] numbers = new int[n];
+
+            Console.WriteLine($"Please enter {n} numbers of the array.");
+
+            for (int i = 0; i < n; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Please enter the number that you are looking for");
+
+            int x = int.Parse(Console.ReadLine());
+
+            int result = 0;
+
+            foreach (var number in numbers)
+            {
+                if (number == x)
+                {
+                    result++;
+                }
+            }
+
+            Console.WriteLine($"The number {x} is {result} times in the array");
         }
     }
 }
---
+++ SumArrays/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SumArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = new int[] { 1, 2, 3, 5, 6 };
+            int[] array2 = new int[] { 6, 6, 7, 2, 3 };
+
+            int[] resultArray = new int[array1.Length];
+
+            //Array.Reverse(array2);
+
+            ReverseIntArray(array2);
+
+            for (int i = 0; i < resultArray.Length; i++)
+            {
+                resultArray[i] = array1[i] + array2[i];
+            }
+
+            Console.WriteLine(string.Join(", ", resultArray));
+        }
+
+        public static void ReverseIntArray(int[] numbers)
+        {
+            int[] reversed = (int[])numbers.Clone();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int x = reversed[i];
+
+                numbers[numbers.Length - 1 - i] = x;
+            }
+        }
+    }
+}
