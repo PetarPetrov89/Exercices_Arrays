f6720e6b77dab262cf663596e1ad96206b9568ca
PetarPetrov89 <62606018+PetarPetrov89@users.noreply.github.com>
2020-04-25 17:50:10 +0300

    Exercices_Arrays

---
+++ 09_ReadMatrix/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _09_ReadMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = new int[3, 3];
+            int sumOfMatrix = 0;
+
+            for (int x = 0; x < matrix.GetLength(0); x++)
+            {
+                for (int y = 0; y < matrix.GetLength(1); y++)
+                {
+                    int element = int.Parse(Console.ReadLine());
+                    sumOfMatrix += element;
+                    matrix[x, y] = element;
+                }
+            }
+
+            for (int x = 0; x < matrix.GetLength(0); x++)
+            {
+                for (int y = 0; y < matrix.GetLength(1); y++)
+                {
+                    Console.Write($"{matrix[x, y]} ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine(sumOfMatrix);
+        }
+    }
+}
---
+++ 10/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+
+namespace _10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+
+            int[][] firstJaggedArray = GetJaggedArray(rows);
+            int[][] secondJaggedArray = GetJaggedArray(rows);
+
+            bool check = IsFittingPossible(firstJaggedArray, secondJaggedArray);
+
+            if (check)
+            {
+                //Принтираме двата общо като цял блок
+                for (int i = 0; i < firstJaggedArray.Length; i++)
+                {
+                    Console.WriteLine($"{string.Join(", ", firstJaggedArray[i].Concat(secondJaggedArray[i]))}");
+                }
+            }
+            else
+            {
+                //Принтираме общия брой елементи на двата jagged масива
+                Console.WriteLine($"Total amount of cells{CalcTotalCells(firstJaggedArray) + CalcTotalCells(secondJaggedArray)}");
+            }
+        }
+
+        private static int CalcTotalCells(int[][] jaggedArray)
+        {
+            int cellsCount = 0;
+
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                cellsCount += jaggedArray[i].Length;
+            }
+
+            return cellsCount;
+        }
+
+        private static bool IsFittingPossible(int[][] firstJaggedArray, int[][] secondJaggedArray)
+        {
+            bool isFit = true;
+
+            int firstRowCellCount = firstJaggedArray[0].Length + secondJaggedArray[0].Length;
+
+            for (int i = 1; i < firstJaggedArray.Length; i++)
+            {
+                if (firstJaggedArray[i].Length + secondJaggedArray[i].Length != firstRowCellCount)
+                {
+                    return false;
+                }
+            }
+
+            return isFit;
+        }
+
+        private static int[][] GetJaggedArray(int rows)
+        {
+            int[][] jaggedArray = new int[rows][];
+
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                jaggedArray[i] = Console.ReadLine()
+                    .Split(' ')
+                    .Select(el => int.Parse(el))
+                    .ToArray();
+            }
+
+            return jaggedArray;
+        }
+    }
+}
